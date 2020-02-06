2761cc91f441cb5ed16d5f3e828668a35d5a136f
Ziro VI <ahmad.forhat@gmail.com>
2020-02-06 15:20:10 -0300

    Tirando as funcionalidades de console.log do sistema para o codigo ficar mais limpo

--- index.js
+++ index.js
@@ -1,16 +1,12 @@
 const decimalUsa = require('@ziro/decimal-usa')
 
-const dataSheets = {values:[["nome","sobrenome","idade"],["Ahmad","Forhat","24"],["João","Silva","30"]]}
-
 const arrayObject = ({ values }) => {
-    // Trocando o nome das variaveis e condicionais
     let respostaMenorQueDois = "Necessário pelo menos 1 cabeçalho e 1 corpo"
     let respostaTypeOf = "Values não foi identificado como objeto"
     let resBodyEqualHeader = "Número de colunas deve ser igual em toda tabela"
-    let tipoObjeto = typeof values == 'object'
-    let temCabecalhoCorpo = values.length >= 2
-    // Começo das condicionais e codigo
+    let tipoObjeto = typeof values === 'object'
     if(!tipoObjeto) throw respostaTypeOf
+    let temCabecalhoCorpo = values.length >= 2
     if(!temCabecalhoCorpo) throw respostaMenorQueDois
     const [header, ...data] = values;
     let columnEqualHeaderBody = header.length === data[0].length
@@ -27,8 +23,5 @@ const arrayObject = ({ values }) => {
       return object
 
 }
-
-console.log(arrayObject(dataSheets))
-
 
 module.exports = arrayObject
