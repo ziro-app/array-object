21f9c080f6040f55ebd3fc7ea9a89e4c67bf3155
Ziro VI <ahmad.forhat@gmail.com>
2020-02-06 17:00:03 -0300

    fazendo os testos de throw no ava

--- test.js
+++ test.js
@@ -11,3 +11,30 @@ test('Receive an normal sheets data', t => {
     console.log(expected)
     t.deepEqual(result, expected)
 })
+
+const empty = {values:1245151}
+
+test('Recive a values without a object', t => {
+	const error = t.throws(() => {
+		arrayObject(empty);
+	}, {instanceOf: TypeError});
+	t.is(error.message, "values deve ser um objeto");
+});
+
+const justHeader = {values:[["a","b","c"]]}
+
+test('Recive header and body', t => {
+	const error = t.throws(() => {
+		arrayObject(justHeader);
+	}, {instanceOf: TypeError});
+	t.is(error.message, "values deve ter um cabeçalho e um corpo");
+});
+
+const differentColumns = {values:[["a","b","c"],["1","2"]]}
+
+test('Recive a diferent column amount', t => {
+	const error = t.throws(() => {
+		arrayObject(differentColumns);
+	}, {instanceOf: TypeError});
+	t.is(error.message, "qtde colunas do cabeçalho deve ser igual a qtde colunas do corpo");
+});
