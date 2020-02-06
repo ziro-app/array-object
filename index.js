4c44f6012e167434b4ab7c5bc549cf88c26aaa75
Ziro VI <ahmad.forhat@gmail.com>
2020-02-06 16:59:37 -0300

    adicionando o type do trhow ao codigo

--- index.js
+++ index.js
@@ -6,10 +6,10 @@ const arrayToObject = ({ values }) => {
   const erro2 = "values deve ter um cabeçalho e um corpo"
   const erro3 = "qtde colunas do cabeçalho deve ser igual a qtde colunas do corpo"
   // Condicionais e codigo
-  if (typeof values !== 'object') throw erro1
-  if (values.length < 2) throw erro2
+  if (typeof values !== 'object') throw new TypeError(erro1)
+  if (values.length < 2) throw new TypeError(erro2)
   const [header, ...data] = values
-  if (header.length !== data[0].length) throw erro3
+  if (header.length !== data[0].length) throw new TypeError(erro3)
   return data.map(row => {
     return Object.fromEntries(
       row.map((column, index) => {
